724fffce62b991eb0406ea455d7d5c34deae1bb9
Sarina Chang <sarinachang7@gmail.com>
2022-04-07 23:47:37 -0400

    dont know how to deal with tag cloud sizing

--- src/components/web-projects/Cloud.js
+++ src/components/web-projects/Cloud.js
@@ -2,14 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function Cloud() {
   const [tc, setTc] = useState({});
+  const [tcSM, setTcSM] = useState({});
 
   useEffect(() => {
     const tc = TagCloud(container, myTags, options);
+    const tcSM = TagCloud(containerSM, myTags, optionsSM);
     setTc(tc);
+    setTcSM(tcSM);
   }, []);
 
   const TagCloud = require('TagCloud');
+
   const container = '.tagcloud';
+  const containerSM = '.tagcloud-small';
+
   const myTags = [
     'JavaScript',
     'CSS',
@@ -26,14 +32,27 @@ function Cloud() {
   ];
 
   const options = {
-    radius: 250,
+    radius: 300,
     maxSpeed: 'fast',
     initSpeed: 'fast',
     direction: 135,
     keep: false,
   };
 
-  return <div className='tagcloud'></div>;
+  const optionsSM = {
+    radius: 100,
+    maxSpeed: 'fast',
+    initSpeed: 'fast',
+    direction: 135,
+    keep: false,
+  };
+
+  return (
+    <div className='cloud'>
+      <div className='tagcloud'></div>
+      <div className='tagcloud-small'></div>
+    </div>
+  );
 }
 
 export default Cloud;
