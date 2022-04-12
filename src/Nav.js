0faa9b7025302c70435fd04dcb3042d1b201820f
Sarina Chang <sarinachang7@gmail.com>
2022-04-12 18:16:47 -0400

    responsive resize suffering

--- src/Nav.js
+++ src/Nav.js
@@ -4,6 +4,7 @@ import './css/Nav.css';
 function Nav() {
   const [show, handleShow] = useState(false);
   const [hamMenu, setHamMenu] = useState(false);
+  const [width, setWidth] = React.useState(window.innerWidth);
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -20,16 +21,39 @@ function Nav() {
     });
   };
 
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', transitionNavBar);
     return () => window.removeEventListener('scroll', transitionNavBar);
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('resize', updateWidthAndHeight);
+    return () => window.removeEventListener('resize', updateWidthAndHeight);
+  }, []);
+
+  useEffect(() => {
+    if (width > 600) {
+      setHamMenu(false);
+    }
+  }, [width]);
+
   return (
-    <div className={`nav ${show && 'nav_black'}`}>
-      <div className='nav_content'>
-        <img className='nav_logo' src='/logo.png' alt='' onClick={scrollTop} />
-        <div className='nav_links'>
+    <div className={`nav ${show && 'nav-black'}`}>
+      <div className='nav-content'>
+        <img
+          className='nav-logo'
+          src='/logo.png'
+          alt=''
+          onClick={() => {
+            scrollTop();
+            setHamMenu(false);
+          }}
+        />
+        <div className='nav-links'>
           <a href='#projects'>
             <p>Projects</p>
           </a>
@@ -42,11 +66,74 @@ function Nav() {
           <a href='#contact'>
             <p>Contact</p>
           </a>
-          <button className='icon' onClick={() => setHamMenu(!hamMenu)}>
+          <button className='ham-icon' onClick={() => setHamMenu(!hamMenu)}>
             &#9776;
           </button>
-          <div className={`ham-menu${hamMenu ? '-visible' : ''}`}>
-            <h1>Links here</h1>
+        </div>
+        <div className={`overlay${hamMenu ? '-visible' : ''}`}></div>
+        <div className={`ham-menu${hamMenu ? '-visible' : ''}`}>
+          <div className='ham-links'>
+            <a href='#projects' onClick={() => setHamMenu(false)}>
+              <p>Projects</p>
+            </a>
+            <hr />
+            <a href='#design' onClick={() => setHamMenu(false)}>
+              <p>Graphic Design</p>
+            </a>
+            <hr />
+
+            <a href='#illustration' onClick={() => setHamMenu(false)}>
+              <p>Illustration</p>
+            </a>
+            <hr />
+
+            <a href='#contact' onClick={() => setHamMenu(false)}>
+              <p>Contact</p>
+            </a>
+            <div className='ham-menu-social'>
+              <a
+                href={'https://github.com/s-arina'}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  role='img'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='red'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='feather feather-github'
+                >
+                  <title>GitHub</title>
+                  <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
+                </svg>
+              </a>{' '}
+              <a
+                href={'https://linkedin.com/in/sarinachang'}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  role='img'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='red'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='feather feather-linkedin'
+                >
+                  <title>LinkedIn</title>
+                  <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
+                  <rect x='2' y='9' width='4' height='12'></rect>
+                  <circle cx='4' cy='4' r='2'></circle>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
